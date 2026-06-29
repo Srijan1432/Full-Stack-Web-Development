a3b3e7626414a6ac991664ba45213199b11e5145
Srijan Kumar <srijan.12324494@lpu.in>
2026-06-29 21:31:58 +0530

    JavaScript

---
+++ javaScript/variable.js
@@ -0,0 +1,34 @@
+let s = 10;
+let b = 3.14;
+console.log(s, b);
+
+const a = 1234;
+console.log(a);
+
+var sri = 10;
+var sri = 20;
+console.log(sri);
+
+
+let str = "I am learning JavaScript";
+console.log(str);
+
+let user;
+console.log(user);
+
+let arr = [3,4,5,5, 'Rohit', true];
+console.log(arr);
+
+let obj = {
+    name : "Srijan",
+    age : 21,
+    year : 4
+}
+console.log(obj);
+
+function add()
+{
+    console.log("Hello");
+}
+add();
+console.log(typeof add);
