642fa9a8db5a1c5ec8475af56d7ab94d71a27db1
Srijan Kumar <srijan.12324494@lpu.in>
2026-06-30 15:11:04 +0530

    operators

---
+++ javaScript/operator.js
@@ -0,0 +1,27 @@
+//Arithmatic Operator
+console.log(5+2);
+console.log(5-2);
+console.log(5*2);
+console.log(5/2);
+console.log(5%2);
+console.log(5**2);
+
+//Assignment Operator.
+let x = 20;
+let y = 10;
+x += y;
+console.log(x);
+
+//Comparision Operator
+let a = 10;
+let b = '10';
+console.log(a > b);
+console.log(b > a);
+console.log(a == b)
+console.log(a >= b);
+console.log(b === a);
+
+//Type Casting
+let ab = 10;
+let ac = String(ab);
+console.log(typeof ac);
